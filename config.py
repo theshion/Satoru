7457261f86e05404bfb5d9c7a77ae589f926fb7c
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-12-27 12:47:29 +0530

    🐾🍁

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "26405281"))
 API_HASH = getenv("API_HASH", "77b54622ef8e0fd15555d939fc74005d")
-BOT_USERNAME = getenv("BOT_USERNAME", "Test_xidbot")
+BOT_USERNAME = getenv("BOT_USERNAME", "papaxdd_bot")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "test_supportchat")
 LOG_CHANNEL = int(getenv("LOG_CHANNEL", "-1001976068632"))
 COMMAND_HANDLER = ["/", "!"]
-BOT_TOKEN = getenv("BOT_TOKEN", "6694970760:AAFv6Zm9Av8HrY7JOTohg0E6c53Ar036eDc")
+BOT_TOKEN = getenv("BOT_TOKEN", "6236671629:AAGUod3DN_hhNITz87h8v8_KEBjZrvAdqRA")
 OWNER_ID = int(getenv("OWNER_ID", "6058139652"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6058139652").split()))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://sano07:sano07@cluster0.jxywgnn.mongodb.net/?retryWrites=true&w=majority")
