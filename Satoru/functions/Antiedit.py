8c5b0c4e3e914ce1f1e67b7383d0db2e53dda203
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2024-02-24 14:29:04 +0530

    Update Antiedit.py

--- Satoru/functions/Antiedit.py
+++ Satoru/functions/Antiedit.py
@@ -38,8 +38,8 @@ async def edited_message_handler(client, message):
     # Check if edit count exceeds the threshold
     if edit_counts[user_id] >= 6:
         # Ban the user
-        await client.kick_chat_member(chat_id=message.chat.id, user_id=user_id)
-        await client.send_message(chat_id=message.chat.id, text=f"User {message.from_user.mention} has been banned for continuously editing messages.")
+        await app.kick_chat_member(chat_id=message.chat.id, user_id=user_id)
+        await app.send_message(chat_id=message.chat.id, text=f"User {message.from_user.mention} has been banned for continuously editing messages.")
         
         # Reset edit count
         edit_counts[user_id] = 0
