d81395131bad0d3eb871fb2d264be58c5785c6bf
Naitik Sharma <147588654+theshion@users.noreply.github.com>
2024-04-04 10:45:02 +0530

    Update Antiedit.py

--- Satoru/functions/Antiedit.py
+++ Satoru/functions/Antiedit.py
@@ -5,14 +5,3 @@ from Satoru import app
 @app.on_edited_message(filters.group & ~filters.me)
 async def delete_edited_messages(client, edited_message):
     await edited_message.delete()
-
-# ----------------------------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------------------------------
-def delete_long_messages(_, m):
-    return len(m.text.split()) > 10
-
-@app.on_message(filters.group & filters.private & delete_long_messages)
-async def delete_and_reply(_, msg):
-    await msg.delete()
-    user_mention = msg.from_user.mention
-    await app.send_message(msg.chat.id, f"Hey {user_mention}, please keep your messages short!")
