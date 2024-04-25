03320d478c0c286be032ab7bab0b89f9c8193284
Naitik Sharma <147588654+theshion@users.noreply.github.com>
2024-04-25 15:22:00 +0530

    Update Antiedit.py

--- Satoru/functions/Antiedit.py
+++ Satoru/functions/Antiedit.py
@@ -1,8 +1,7 @@
 from pyrogram import Client, filters
+import logging
 import os
 from pyrogram.types import *
-from pyrogram import filters
-from satoru import pbot as app
 from pyrogram.errors import FloodWait
 
 # -------------------------------
@@ -24,25 +23,25 @@ def size_formatter(bytes: int) -> str:
 
 FORBIDDEN_KEYWORDS = ["porn", "xxx", "sex", "NCERT", "XII", "page", "Ans", "meiotic", "divisions", "System.in", "Scanner", "void", "nextInt"]
 
+app = Client("my_bot")
+
 @app.on_message()
 async def handle_message(client, message):
     if any(keyword in message.text for keyword in FORBIDDEN_KEYWORDS):
-        logging.info(f"✦ ᴅᴇʟᴇᴛɪɴɢ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ ɪᴅ {message.id}")
+        logging.info(f"✦ ᴅᴇʟᴇᴛɪɴɢ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ ɪᴅ {message.message_id}")
         await message.delete()
-        user_mention = from_user.mention
+        user_mention = message.from_user.mention
         await message.reply_text(f"✦ ʜᴇʏ {user_mention}, ʙᴀʙʏ ᴅᴏɴ'ᴛ sᴇɴᴅ ɴᴇxᴛ ᴛɪᴍᴇ.")
-    if any(keyword in message.caption for keyword in FORBIDDEN_KEYWORDS):
-        logging.info(f"✦ ᴅᴇʟᴇᴛɪɴɢ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ ɪᴅ {message.id}")
+    if message.caption and any(keyword in message.caption for keyword in FORBIDDEN_KEYWORDS):
+        logging.info(f"✦ ᴅᴇʟᴇᴛɪɴɢ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ ɪᴅ {message.message_id}")
         await message.delete()
-        user_mention = from_user.mention
+        user_mention = message.from_user.mention
         await message.reply_text(f"✦ ʜᴇʏ {user_mention}, ʙᴀʙʏ ᴅᴏɴ'ᴛ sᴇɴᴅ, ɴᴇxᴛ ᴛɪᴍᴇ.")
-        
-#  -----------------------------------------------------------
+
 @app.on_edited_message(filters.group & ~filters.me)
 async def delete_edited_messages(client, edited_message):
     await edited_message.delete()
 
-# ------------------------------------------------------------
 def delete_long_messages(_, m):
     return len(m.text.split()) > 400
 
@@ -51,16 +50,15 @@ async def delete_and_reply(_, msg):
     await msg.delete()
     user_mention = msg.from_user.mention
     await app.send_message(msg.chat.id, f"✦ ʜᴇʏ {user_mention} ʙᴀʙʏ, ᴘʟᴇᴀsᴇ ᴋᴇᴇᴘ ʏᴏᴜʀ ᴍᴇssᴀɢᴇ sʜᴏʀᴛ.")
-     #-----------------------------------------------------------
 
 async def delete_pdf_files(client, message):
     if message.document and message.document.mime_type == "application/pdf":
-        warning_message = f"✦ ʜᴇʏ {user_mention} ᴅᴏɴ'ᴛ sᴇɴᴅ ᴘᴅғ ғɪʟᴇs ʙᴀʙʏ, ғᴏʀ ᴄᴏᴘʏʀɪɢʜᴛ ᴄʟɪᴍʙ."
+        warning_message = f"✦ ʜᴇʏ {message.from_user.mention} ᴅᴏɴ'ᴛ sᴇɴᴅ ᴘᴅғ ғɪʟᴇs ʙᴀʙʏ, ғᴏʀ ᴄᴏᴘʏʀɪɢʜᴛ ᴄʟɪᴍʙ."
         await message.reply_text(warning_message)
         await message.delete()
-    else:  
-        pass
 
 @app.on_message(filters.group & filters.document)
 async def message_handler(client, message):
     await delete_pdf_files(client, message)
+
+app.run()
